df8f97f80328e41f0150c6b1d24c1ec5f91bc788
JoszkinKevin <54977279+JoszkinKevin@users.noreply.github.com>
2023-11-29 15:20:33 +0100

    Update faszgeci.py

--- faszgeci.py
+++ faszgeci.py
@@ -7,14 +7,14 @@ def main():
 
   muvelet = st.selectbox("Pénznem: ",("dollár","euro"))
 
-  dollar = "0,0029"
-  euro = "0,0026" 
+  dollar = 0,0029
+  euro = 0,0026 
 
   result = 0
   if muvelet == "dollár":
     result = szam1 * dollar
   elif muvelet == "euro":
-    result = szam1 * dollar
+    result = szam1 * euro
   st.write(f"Eredmény: {result}")
 
 if __name__ == '__main__':
