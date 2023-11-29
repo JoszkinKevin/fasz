b47e521de826e731aebc2736b736c300662c5b45
JoszkinKevin <54977279+JoszkinKevin@users.noreply.github.com>
2023-11-29 15:11:59 +0100

    Update faszgeci.py

--- faszgeci.py
+++ faszgeci.py
@@ -1,1 +1,20 @@
 import streamlit as st
+
+def main():
+  st.title("Valutaváltás")
+
+  szam1 = st.number_input(label="Mennyi pénz: ")
+
+  muvelet = st.selectbox("Pénznem: ",("dollár","euro"))
+
+  
+
+  result = 0
+  if muvelet == "dollár":
+    result = szam1 * 3
+  elif muvelet == "euro":
+    result = szam1 * 4
+  st.write(f"Eredmény: {result}")
+
+if __name__ == '__main__':
+  main()
